84caf39b63481d590006b6d3049088083865621e
classicwuhao@gmail.com <classicwuhao@gmail.com>
2018-05-13 15:14:40 +0100

    update paper

--- src/testDFA.java
+++ src/testDFA.java
@@ -34,7 +34,7 @@ public class testDFA{
 		
 		DFA dfa = new DFA ("abb_from_dragon_book_p_120",t,c,0,fs);
 		System.out.println(dfa);
-		Cyclone cyclone = new Cyclone(dfa,7);
+		Cyclone cyclone = new Cyclone(dfa,20);
 		cyclone.Enumerate();
 	}
 
