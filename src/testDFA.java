16902ce93f3b6722f53cd55a55ce9588fef9f6ba
classicwuhao <classicwuhao@gmail.com>
2018-05-16 17:34:19 +0100

    adding more test cases.

--- src/testDFA.java
+++ src/testDFA.java
@@ -3,6 +3,7 @@ public class testDFA{
 	public static void main (String args[]){
 		Case1();
 		Case2();
+		Case3();
 	}
 
 	public static void Case1(){
@@ -19,7 +20,7 @@ public class testDFA{
 	}
 
 	/**
-	 * From dragon book (1st edition), p120.
+	 * From the dragon book (1st edition), p120.
 	 */
 	public static void Case2(){
 		int[][] t={
@@ -34,8 +35,27 @@ public class testDFA{
 		
 		DFA dfa = new DFA ("abb_from_dragon_book_p_120",t,c,0,fs);
 		System.out.println(dfa);
-		Cyclone cyclone = new Cyclone(dfa,20);
+		Cyclone cyclone = new Cyclone(dfa,5);
 		cyclone.Enumerate();
 	}
 
+	/**
+	 * From Sipser's book, p76. A three-state DFA
+	 */
+	public static void Case3(){
+		int[][] t={
+			{1,2},
+			{0,1},
+			{1,0}
+		};
+	
+		char[] c={'a','b'};
+		int[] fs={1,2};
+	
+		DFA dfa = new DFA ("three_state_from_sipser's_book_p_76",t,c,0,fs);
+		System.out.println(dfa);
+		Cyclone cyclone = new Cyclone(dfa,3);
+		cyclone.Enumerate();
+		
+	}	
 }
