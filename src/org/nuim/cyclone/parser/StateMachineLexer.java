51dbabfb137eeeb048cd2dfad2e7a5857493fb10
classicwuhao <classicwuhao@gmail.com>
2020-07-31 17:06:08 +0800

    1.SpecTypes, 2.Semantics for where clause in transition, 3.State Accessor, 4. Context update

--- src/org/nuim/cyclone/parser/StateMachineLexer.java
+++ src/org/nuim/cyclone/parser/StateMachineLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.4 StateMachine.g 2020-07-31 17:03:15
+// $ANTLR 3.4 StateMachine.g 2020-07-31 17:05:10
 
 package org.nuim.cyclone.parser;
 
--- src/org/nuim/cyclone/parser/StateMachineParser.java
+++ src/org/nuim/cyclone/parser/StateMachineParser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.4 StateMachine.g 2020-07-31 17:03:14
+// $ANTLR 3.4 StateMachine.g 2020-07-31 17:05:08
 
 package org.nuim.cyclone.parser;
 import java.util.Collections;
